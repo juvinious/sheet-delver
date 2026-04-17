13901fbcecc7e9df6eed1942a7f9603b7dbf3585
juvinious <581120+juvinious@users.noreply.github.com>
2026-04-17 16:11:53 -0400

    Added unit tests for socket gateway and realtime broadcast.

--- src/tests/unit/run.ts
+++ src/tests/unit/run.ts
@@ -1,10 +1,20 @@
 import { run as runStatusSanitize } from './status-sanitize.test';
 import { run as runLocalhostPolicy } from './localhost-policy.test';
+import { run as runRealtimeBroadcaster } from './realtime-broadcaster.test';
+import { run as runAppSocketGateway } from './app-socket-gateway.test';
 
-function runAllUnitTests() {
+async function runAllUnitTests() {
     runStatusSanitize();
     runLocalhostPolicy();
+    await runRealtimeBroadcaster();
+    await runAppSocketGateway();
 }
 
-runAllUnitTests();
-console.log('unit test suite passed');
+runAllUnitTests()
+    .then(() => {
+        console.log('unit test suite passed');
+    })
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
